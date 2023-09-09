3049aa7c3855a7b42a81dc1b6f7980ab83779359
yoyc543 <yoyc54@gmail.com>
2023-09-09 17:21:19 -0500

    se hiso algo aa

--- Gestor_Citas_Medicas/src/gestor_citas_medicas/Gestor_Citas_Medicas.java
+++ Gestor_Citas_Medicas/src/gestor_citas_medicas/Gestor_Citas_Medicas.java
@@ -14,10 +14,36 @@ public class Gestor_Citas_Medicas {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("HOLA MUNDO");
-        System.out.println("HOLA MUNDO");
-        System.out.println("HOLA MUNDO");
+        System.out.println("hello word");
+    Especialidad especialidad1 = new Especialidad(1,"Cardiologia");
+    Medico medico1= new Medico(1,especialidad1, "cedula1", "Luis", 
+            "Rojas", 54697812, "Masculino", 978375159, "S/N");
+//      paciente1.ingresarIDPaciente(111);
+//      paciente1.ingresarDNI(12659854);
+//      paciente1.ingresarPaciente("Juan");
+//      paciente1.ingresarApellido("Ortiz");
+//      paciente1.ingresarSexo("Masculino");
+//      paciente1.ingresarCelular(789546132);
+//      paciente1.ingresarDireccion("dkhjlashdkabcxzn");
+//      paciente1.mostrarIDPaciente();
+//      paciente1.mostrarDNI();
+//      paciente1.mostrarPaciente();
+//      paciente1.mostrarApellido();
+//      paciente1.mostrarSexo();
+//      paciente1.mostrarCelular();
+//      paciente1.mostrarDireccion();
+//      
+//        System.out.println(paciente1.mostrarIDPaciente());
+//        System.out.println( paciente1.mostrarDNI());
+//        System.out.println(paciente1.mostrarPaciente());
+//        System.out.println(paciente1.mostrarApellido());
+//        System.out.println(paciente1.mostrarSexo());
+//        System.out.println(paciente1.mostrarCelular());
+//        System.out.println(paciente1.mostrarDireccion());
+        
+        
+        System.out.println( medico1.toString());
     }
+    
     
 }
---
+++ Gestor_Citas_Medicas/src/gestor_citas_medicas/Paciente.java
@@ -0,0 +1,125 @@
+package citasreserva;
+
+
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+/**
+ *
+ * @author PC01
+ */
+public class Paciente {
+    private int idPaciente;
+    private int DNI;
+    private String Nombre;
+    private String Apellido;
+    private String Sexo;
+    private int Celular;
+    private String Direccion;
+//    
+//    public void ingresarIDPaciente(int nuevoIDPaciente){
+//        this.idPaciente = nuevoIDPaciente;
+//    }
+//    public int mostrarIDPaciente(){
+//        return this.idPaciente;
+//    }
+//    public void ingresarDNI(int nuevoDNI){
+//        this.DNI = nuevoDNI;
+//    }
+//    public int mostrarDNI(){
+//        return this.DNI;
+//    }
+//        public void ingresarPaciente(String nuevoPaciente){
+//        this.Nombre =  nuevoPaciente;
+//    }
+//    public String mostrarPaciente(){
+//        return this.Nombre;
+//    }
+//    public void ingresarApellido(String nuevoApellido){
+//        this.Apellido = nuevoApellido;
+//    }
+//    public String mostrarApellido(){
+//        return this.Apellido;
+//    }
+//    public void ingresarSexo(String nuevoSexo){
+//        this.Sexo = nuevoSexo;
+//    }
+//    public String mostrarSexo(){
+//        return this.Sexo;
+//    }
+//    public void ingresarCelular(int nuevoCelular){
+//        this.Celular = nuevoCelular;
+//    }
+//    public int mostrarCelular(){
+//        return this.Celular;
+//    }
+//    public void ingresarDireccion(String nuevoDireccion){
+//        this.Direccion = nuevoDireccion;
+//    }
+//    public String mostrarDireccion(){
+//        return this.Direccion;
+//    }
+
+    public int getIdPaciente() {
+        return idPaciente;
+    }
+
+    public void setIdPaciente(int idPaciente) {
+        this.idPaciente = idPaciente;
+    }
+
+    public int getDNI() {
+        return DNI;
+    }
+
+    public void setDNI(int DNI) {
+        this.DNI = DNI;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getApellido() {
+        return Apellido;
+    }
+
+    public void setApellido(String Apellido) {
+        this.Apellido = Apellido;
+    }
+
+    public String getSexo() {
+        return Sexo;
+    }
+
+    public void setSexo(String Sexo) {
+        this.Sexo = Sexo;
+    }
+
+    public int getCelular() {
+        return Celular;
+    }
+
+    public void setCelular(int Celular) {
+        this.Celular = Celular;
+    }
+
+    public String getDireccion() {
+        return Direccion;
+    }
+
+    public void setDireccion(String Direccion) {
+        this.Direccion = Direccion;
+    }
+    
+    
+    
+}
